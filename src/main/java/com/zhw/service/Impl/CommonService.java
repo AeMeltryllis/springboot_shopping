021077426bbfb6c3fb90e411f7ad5b5bb2b052a9
ZHW <429597940@qq.com>
2019-12-23 17:46:29 +0800

    实现分页/准备实现文件上传 https://spring.io/guides/gs/uploading-files/

--- src/main/java/com/zhw/service/Impl/CommonService.java
+++ src/main/java/com/zhw/service/Impl/CommonService.java
@@ -8,18 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Service
+@Transactional
 public class CommonService implements ICommonService {
+
+    final  static int PAGE_SIZE=10;
+
+
     @Autowired
     ICategoryDO iCategoryDO;
 
     @Override
     public List<CategoryInfoPO> listCategory() {
-        return iCategoryDO.findAll();
+        Sort sort = Sort.by(Sort.Direction.DESC,"id");
+        return iCategoryDO.findAll(sort);
     }
 
     @Override
@@ -27,9 +39,25 @@ public class CommonService implements ICommonService {
         return iCategoryDO.getById(id);
     }
 
+
     @Override
-    public Page pageCategory() {
-        return null;
+    public Page pageCategory(int pageIndex,int size) {
+        Sort sort = Sort.by(Sort.Direction.DESC,"id");
+        //Page类中的下标是从0开始，所以穿进来的pageIndex要-1
+        Pageable  page=  PageRequest.of(pageIndex-1, size,sort);
+        return iCategoryDO.findAll(page);
     }
+
+    @Override
+    public void addCategory(CategoryInfoPO category) {
+        iCategoryDO.save(category);
+    }
+
+    @Override
+    public void saveImage(MultipartFile image, HttpServletRequest request) {
+
+
+    }
+
 
 }
